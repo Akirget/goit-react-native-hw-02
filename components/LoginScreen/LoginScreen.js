403dc3e79d290dd403b794749479ca3eb5d1399e
Kirhet Oleksandr <kirget.sasha2002@gmail.com>
2023-02-09 09:52:32 +0100

    Домашнє завдання №2 React Native

---
+++ components/LoginScreen/LoginScreen.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import React from "react";
+
+const LoginScreen = () => {
+  return (
+    <View styles={styles.container}>
+      {/* <ImageBackground
+        styles={styles.bg}
+        source={require("../../assets/images/bgImage.png")}
+      ></ImageBackground> */}
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  //   container: {
+  //     flex: 1,
+  //     backgroundColor: "#fff",
+  //     alignItems: "center",
+  //     justifyContent: "center",
+  //   },
+  //   bg: { flex: 1, reziseMode: "cover" },
+});
---
+++ components/RegistrationScreen/RegistrationScreen.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+
+export default function RegistrationScreen() {
+  return (
+    <View styles={styles.container}>
+      {/* <Text>asdfasdf</Text> */}
+      {/* <ImageBackground
+        styles={styles.bg}
+        source={require("../../assets/images/bgImage.png")}
+      >
+        <Text>
+          dsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsf
+        </Text>
+      </ImageBackground> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 2,
+    borderColor: "red",
+  },
+  bg: { flex: 1, resizeMode: "cover" },
+});
